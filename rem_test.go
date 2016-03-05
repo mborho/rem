7a0fbade985635500949ecd09afa35312a30cc3f
Martin Borho <martin@borho.net>
2016-03-05 18:37:56 +0100

    Added test for printing lines.

--- rem_test.go
+++ rem_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"io/ioutil"
+	"os"
 	"testing"
 )
 
@@ -18,7 +19,7 @@ func removeRemFile(r *Rem) {
 }
 
 func getTestRem(t *testing.T) *Rem {
-	cmds := []byte("ls\n#foo#bla\n")
+	cmds := []byte("ls\n#foo#ls -la\n")
 	if err := ioutil.WriteFile(remfile, cmds, 0644); err != nil {
 		t.Fatalf("Cannot create rem testfile, %s", err)
 	}
@@ -50,5 +51,57 @@ func TestRead(t *testing.T) {
 
 	if rem.file == nil {
 		t.Error("File not opened")
+	}
+
+}
+
+func TestPrintLine(t *testing.T) {
+	rescueStdout := os.Stdout
+	r, w, _ := os.Pipe()
+	os.Stdout = w
+
+	rem := getTestRem(t)
+	defer removeRemFile(rem)
+	rem.read()
+
+	rem.printLine(1)
+
+	w.Close()
+	out, _ := ioutil.ReadAll(r)
+	os.Stdout = rescueStdout
+
+	if string(out) != "ls -la\n" {
+		t.Errorf("Wrong line output, got %s", out)
+	}
+}
+
+func TestPrintLineError(t *testing.T) {
+	rem := getTestRem(t)
+	defer removeRemFile(rem)
+	rem.read()
+
+	err := rem.printLine(2)
+	if err == nil {
+		t.Error("No error when calling non existant line.")
+	}
+}
+
+func TestPrintAllLines(t *testing.T) {
+	rescueStdout := os.Stdout
+	r, w, _ := os.Pipe()
+	os.Stdout = w
+
+	rem := getTestRem(t)
+	defer removeRemFile(rem)
+	rem.read()
+
+	rem.printAllLines()
+
+	w.Close()
+	out, _ := ioutil.ReadAll(r)
+	os.Stdout = rescueStdout
+
+	if string(out) != " 0   -   ls\n 1  foo  ls -la\n" {
+		t.Errorf("Wrong line output, got %s", out)
 	}
 }
